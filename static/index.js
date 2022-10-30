4eeea96c10c4a2ca0dee150de77b5446a84a3dd4
Айяр Халилов <super.ayar-04@yandex.ru>
2022-10-30 18:21:45 +0300

    v1

--- static/index.js
+++ static/index.js
@@ -1,30 +1,58 @@
-function renderRoute(route = window.location.pathname) {
-    switch (route) {
-        case '/feedback': {
-            MicroModal.show('feedbackModal');
-            break;
-        }
-        // for '/' and others
-        default: {
-            MicroModal.close('feedbackModal');
-            break;
-        }
-    }
-}
+document.addEventListener('DOMContentLoaded', () => {
 
-window.addEventListener("popstate", (popStateEvent) => {
-    renderRoute(popStateEvent.state.route)
-});
+    let feedbackModalIsShown = false;
 
-document.addEventListener("DOMContentLoaded", () => {
-    MicroModal.init({
-        onShow: modal => history.pushState({route: '/feedback'}, "", '/feedback'),
-        onClose: modal => history.back(),
-        openClass: "show",
-        awaitOpenAnimation: true,
-        awaitCloseAnimation: true
+    const feedbackModalEl = document.querySelector('#feedback-modal');
+    const feedbackModal = new bootstrap.Modal(feedbackModalEl);
+
+    const feedbackFormEl = feedbackModalEl.querySelector('form');
+    const feedbackFormCacher = createFormCacher(feedbackFormEl);
+    feedbackFormCacher.backup();
+
+    feedbackModalEl.addEventListener('show.bs.modal', () => {
+        feedbackModalIsShown = true;
+        history.pushState(null, '', '/feedback');
+    });
+    feedbackModalEl.addEventListener('shown.bs.modal', () => {
+        feedbackModalEl.querySelector('input, select, textarea').focus();
+    });
+    feedbackModalEl.addEventListener('hide.bs.modal', () => {
+        if (!feedbackModalIsShown) return;
+
+        history.back();
     });
 
-    // initial render;
-    renderRoute();
+    feedbackFormEl.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        feedbackFormEl.classList.add('was-validated')
+        if (!feedbackFormEl.checkValidity()) {
+            e.stopPropagation();
+            return false;
+        }
+
+        const formData = new FormData(feedbackFormEl);
+
+        // Convert FormData to plain JavaScript object
+        const vals = Object.fromEntries(formData.entries());
+
+        const waitResponse = fetch(feedbackFormEl.getAttribute('action'), {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json', 'Accept': 'application/json' },
+            body: JSON.stringify(vals)
+        });
+
+        waitResponse.then(() => {
+            pushSuccessToast('Отзыв успешно отправлен, спасибо!');
+            feedbackFormCacher.clear();
+        }).catch(() => {
+            pushFailureToast('Произошла неизвестная ошибка, приносим свои извинения за предоставленные неудобства.');
+        });
+    });
+
+    window.addEventListener('popstate', () => {
+        feedbackModalIsShown = false;
+        feedbackModal.hide();
+    });
+
 });
